2cef87beb9e92f59e54961c4f202d548a2425475
Sourav Das <iamsouravdaswade420@gmail.com>
2021-03-13 20:30:29 +0530

    adding item , showing item , editing item with dynamic liks

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,28 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolios=Portfolio.all
+  end
+  def new
+    @portfolio=Portfolio.new
+  end
+  def create
+    @portfolioI=Portfolio.new(portfolio_params)
+    if @portfolioI.save
+      redirect_to portfolios_path
+    end
+
+  end
+  def portfolio_params
+    params.require(:portfolio).permit(:title,:subtitle)
+  end
+  def edit
+    @portfolio=Portfolio.find(params[:id])
+  end
+  def update
+    @portfolio=Portfolio.find(params[:id])
+    if @portfolio.update(portfolio_params)
+      redirect_to portfolios_path
+
+    end
+  end
 end
