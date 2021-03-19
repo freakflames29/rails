b6869774445e36b83d73f76ac0ee96d7330fff8d
Sourav Das <iamsouravdaswade420@gmail.com>
2021-03-19 11:13:53 +0530

    added some routes

--- app/controllers/crud_controller.rb
+++ app/controllers/crud_controller.rb
@@ -1,5 +1,31 @@
 class CrudController < ApplicationController
   def index
+  	@names=Crud.all
 
   end
+  def new
+  	@name=Crud.new
+
+
+  end
+  def create
+  	@name=Crud.new(crud_param)
+  	if @name.save
+  		redirect_to index_path
+  	end
+
+  end
+  def destroy
+
+  end
+  def update
+
+  end
+  def show
+
+  end
+  private
+  	def crud_param
+  		params.require(:name).permit(:name,:title,:age)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
 
 
   get 'crud' ,to:"crud#index", :as=>"index" # as is used for the prefix
-  get 'crud/edit', to:"crud#index" :as=>"edit"
+  get 'crud/new' ,to: "crud#new" ,:as=>"new"
+  # get 'crud/edit', to:"crud#edit", :as=>"edit"
+  post 'crud/create', to:"crud#create" 
+  get 'crud/edit/:id', to: "crud#edit"
 
+  get 'crud/show/:id', to:"crud#show",:as=>"show"
+  delete 'crud/show/:id', to:"crud#destroy"
+  put 'crud/show/:id', to:"crud#update"
+   
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
