8177d49d238aa11afe6a1ab4fbcf4ac6559e2bc1
Sourav Das <iamsouravdaswade420@gmail.com>
2021-03-23 15:32:52 +0530

    Added password mailer created a token and uses in our mailer views

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,7 +3,18 @@ class PasswordResetsController < ApplicationController
 
 	end
 	def create
+		@user=Registration.find_by(email:params[:email])
 
+		if @user.present?
+
+			#mailer
+			PasswordMailer.with(user: @user).reset.deliver_now
+
+			redirect_to root_path,notice:"Password reset link sent to the email"
+		else
+			flash[:alert]="Invalid email"
+			render :new
+		end
 		
 	end
 end
